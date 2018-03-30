ecd7567305b351f98596c53c978e1c8a71514e89
pavel <pavel.ismailov@gmail.com>
2018-03-30 17:29:33 +0700

    Added some functions

--- app/src/main/java/com/tarrk/kotlin_couroutines_example/MainActivity.kt
+++ app/src/main/java/com/tarrk/kotlin_couroutines_example/MainActivity.kt
@@ -2,12 +2,11 @@ package com.tarrk.kotlin_couroutines_example
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
-import kotlinx.coroutines.experimental.CompletableDeferred
-import kotlinx.coroutines.experimental.Deferred
-import kotlinx.coroutines.experimental.async
-import kotlinx.coroutines.experimental.delay
+import kotlinx.coroutines.experimental.*
 import kotlin.concurrent.thread
 import kotlin.coroutines.experimental.RestrictsSuspension
+import kotlin.coroutines.experimental.SequenceBuilder
+import kotlin.coroutines.experimental.buildSequence
 
 class MainActivity : AppCompatActivity() {
 
@@ -20,7 +19,7 @@ class MainActivity : AppCompatActivity() {
             L.plant(L.DebugTree())
         }
 
-        four()
+        five()
 
     }
 
@@ -83,6 +82,60 @@ class MainActivity : AppCompatActivity() {
 
     }
 
+    private fun five() {
+        L.i("five")
+        val fibonacciSeq = buildSequence {
+            var a = 0
+            var b = 1
+
+            yield(1)
+
+            while (true) {
+                yield(a + b)
+
+                val tmp = a + b
+                a = b
+                b = tmp
+            }
+        }
+
+        // Print the first five Fibonacci numbers
+        println(fibonacciSeq.take(8).toList())
+    }
+
+    private fun six() {
+        L.i("six")
+        val lazySeq = buildSequence {
+            print("START ")
+            for (i in 1..5) {
+                yield(i)
+                print("STEP ")
+            }
+            print("END")
+        }
+
+        // Print the first three elements of the sequence
+        lazySeq.take(3).forEach { print("$it ") }
+    }
+
+    private fun seven() {
+        L.i("seven")
+        val lazySeq = buildSequence {
+            yield(0)
+            yieldAll(1..10)
+        }
+
+        lazySeq.forEach { print("$it ") }
+    }
+
+    private fun eight() {
+        L.i("eight")
+        val lazySeq = buildSequence {
+            for (i in 1..10) yieldIfOdd(i)
+        }
+        lazySeq.forEach { print("$it ") }
+    }
+
     private suspend fun doSomething(foo: String): Deferred<String> {
         delay(1000L)
         L.i("World!")
@@ -91,6 +144,10 @@ class MainActivity : AppCompatActivity() {
 
     private fun <T> asyncCustom(block: suspend () -> T) {
 
+    }
+
+    private suspend fun kotlin.coroutines.experimental.SequenceBuilder<Int>.yieldIfOdd(x: Int) {
+        if (x % 2 != 0) yield(x)
     }
 
     /***/
@@ -113,3 +170,7 @@ class MainActivity : AppCompatActivity() {
     }
 
 }
+
+
+
+
